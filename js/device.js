f594fddc44fa55b174dd3daf5c1c2eed5c55ad1a
am250648 <eng_amin89@hotmail.com>
2022-02-19 14:58:35 +0200

    Update device.js

--- js/device.js
+++ js/device.js
@@ -85,7 +85,7 @@ handlePermission();
     document.getElementById(fieldName).innerHTML = value.toFixed(precision);
 	}
 	
-   let acl = new Accelerometer({frequency: 60});
+   let acl = new Accelerometer({frequency: 10});
 acl.addEventListener('reading', () => {
   console.log("Acceleration along the X-axis " + acl.x);
   console.log("Acceleration along the Y-axis " + acl.y);
