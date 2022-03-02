fcd16f3d2a5a9ff908460b1afef2e492489de177
am250648 <eng_amin89@hotmail.com>
2022-03-02 20:14:39 +0200

    Update device.js

--- js/device.js
+++ js/device.js
@@ -1,5 +1,52 @@
 
    //handle permisiion
+   // set up the page
+
+var mapCanvas = document.getElementById('map_canvas');
+var geoBtn = document.querySelector('.enable');
+var revokeBtn = document.querySelector('.revoke');
+
+
+
+// draw the google map, or not
+
+  
+var positionDenied = function() {
+  geoBtn.style.display = 'inline';
+};
+  
+var revealPosition = function(position) {
+  geoBtn.style.display = 'none';
+  var markerTitle = "You are here";
+
+  var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+  var myOptions = {
+    zoom: 16,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  
+  var map = new google.maps.Map(mapCanvas, myOptions);
+
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map,
+    title: markerTitle
+  });
+}
+
+// test for geolocation support, provide geolocation settings, determine location of the user's device
+
+
+if (!"geolocation" in navigator) {
+  alert("No geolocation available!");
+}
+  
+var geoSettings = {
+  enableHighAccuracy: false,
+  maximumAge        : 30000,
+  timeout           : 20000
+};
 
 // Start everything off
 
